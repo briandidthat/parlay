91454db3bbf9f5f58328ce08e763cadcb4a5c7c9
brianthf <blora616@gmail.com>
2020-10-27 15:55:15 -0400

    fix inputs

--- api/models.py
+++ api/models.py
@@ -2,9 +2,25 @@ from werkzeug.security import generate_password_hash
 from api import db
 
 class User(db.Model):
+    """
+    Returns a user representation class.
+    - serialize() returns a dictionary representation of class attributes for jsonification.
+    """
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True, nullable=False)
     username = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
     role = db.Column(db.String(50), nullable = False)
+
+    def __init__(self, email, username, password):
+        self.email = email
+        self.username = username
+        self.password = generate_password_hash(password)
+
+    def serialize(self):
+        return {
+            "user_id": self.id,
+            "username": self.username
+        }
+
