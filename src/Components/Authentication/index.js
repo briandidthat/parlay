df9320f29148b3ce924dd5714f1767da82df5446
brianthf <blora616@gmail.com>
2020-10-26 22:35:23 -0400

    add formik &&  yup forms for validation

--- src/Components/Authentication/index.js
+++ src/Components/Authentication/index.js
@@ -1,2 +1,86 @@
 import React from "react";
+import { Formik, Form, useField } from "formik";
+import { RegisterSchema, LoginSchema } from "../../utils/Validation";
 
+const CustomField = ({ label, ...props }) => {
+  const [field, meta, helpers] = useField(props);
+
+  return (
+    <>
+      <label>
+        {label}
+        <input {...field} {...props} />
+      </label>
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
+    </>
+  );
+};
+
+export function LoginForm() {
+  return (
+    <>
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        validationSchema={LoginSchema}
+        validateOnChange={true}
+        onSubmit={(values, actions) => {
+          alert(JSON.stringify(values, null, 2));
+          actions.setSubmitting(false);
+        }}
+      >
+        {() => (
+          <Form>
+            <CustomField
+              name="username"
+              component={CustomField}
+              placeholder="text"
+            />
+            <CustomField
+              name="password"
+              component={CustomField}
+              placeholder="password"
+            />
+          </Form>
+        )}
+      </Formik>
+    </>
+  );
+}
+
+export function RegistrationForm() {
+  return (
+    <>
+      <Formik
+        initialValues={{ email: "", username: "", password: "" }}
+        validationSchema={RegisterSchema}
+        validateOnChange={true}
+        onSubmit={(values, actions) => {
+          alert(JSON.stringify(values, null, 2));
+          actions.setSubmitting(false);
+        }}
+      >
+        {() => (
+          <Form>
+            <CustomField
+              name="email"
+              component={CustomField}
+              placeholder="email"
+            />
+            <CustomField
+              name="username"
+              component={CustomField}
+              placeholder="text"
+            />
+            <CustomField
+              name="password"
+              component={CustomField}
+              placeholder="password"
+            />
+          </Form>
+        )}
+      </Formik>
+    </>
+  );
+}
