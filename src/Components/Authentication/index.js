09344520b328e4799b8be1a53e88b0254a5c5c66
brianthf <blora616@gmail.com>
2020-10-29 00:22:43 -0400

    migrate to bootstrap

--- src/Components/Authentication/index.js
+++ src/Components/Authentication/index.js
@@ -1,104 +1,128 @@
 import React from "react";
-import { Formik, Form, useField } from "formik";
+import { Formik, Form } from "formik";
+import { CustomField } from "../Input";
 import { RegisterSchema, LoginSchema } from "../../utils/Validation";
-import "./auth.css";
-
-const CustomField = ({ label, ...props }) => {
-  const [field, meta] = useField(props);
-
-  return (
-    <>
-      <label>
-        {label}
-        <input {...field} {...props} />
-      </label>
-      {meta.touched && meta.error ? (
-        <div className="error">{meta.error}</div>
-      ) : null}
-    </>
-  );
-};
+import { Col, Container, Row } from "react-bootstrap";
 
 export function LoginForm() {
   return (
-    <>
-      <Formik
-        initialValues={{ username: "", password: "" }}
-        validationSchema={LoginSchema}
-        validateOnChange={true}
-        onSubmit={(values, actions) => {
-          alert(JSON.stringify(values, null, 2));
-          actions.setSubmitting(false);
-        }}
-      >
-        {() => (
+    <Formik
+      initialValues={{ username: "", password: "" }}
+      validationSchema={LoginSchema}
+      validateOnChange={true}
+      onSubmit={(values, actions) => {
+        alert(JSON.stringify(values, null, 2));
+        actions.setSubmitting(false);
+      }}
+    >
+      {() => (
+        <Container fluid style={{ textAlign: "center" }}>
           <Form>
-            <CustomField
-              className="field"
-              name="username"
-              component={CustomField}
-              placeholder="username"
-              type="username"
-            />
-            <CustomField
-              className="field"
-              name="password"
-              component={CustomField}
-              placeholder="password"
-              type="password"
-            />
+            <Row>
+              <Col>
+                <h1>Login</h1>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <CustomField
+                  className="field"
+                  name="username"
+                  component={CustomField}
+                  placeholder="username"
+                  type="username"
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <CustomField
+                  className="field"
+                  name="password"
+                  component={CustomField}
+                  placeholder="password"
+                  type="password"
+                />
+              </Col>
+            </Row>
           </Form>
-        )}
-      </Formik>
-    </>
+        </Container>
+      )}
+    </Formik>
   );
 }
 
 export function RegistrationForm() {
   return (
-    <>
-      <Formik
-        initialValues={{ email: "", username: "", password: "", confirmPassword: "" }}
-        validationSchema={RegisterSchema}
-        validateOnChange={true}
-        onSubmit={(values, actions) => {
-          alert(JSON.stringify(values, null, 2));
-          actions.setSubmitting(false);
-        }}
-      >
-        {() => (
+    <Formik
+      initialValues={{
+        email: "",
+        username: "",
+        password: "",
+        confirmPassword: "",
+      }}
+      validationSchema={RegisterSchema}
+      validateOnChange={true}
+      onSubmit={(values, actions) => {
+        alert(JSON.stringify(values, null, 2));
+        actions.setSubmitting(false);
+      }}
+    >
+      {() => (
+        <Container fluid style={{ textAlign: "center" }}>
           <Form>
-            <CustomField
-              label="Email"
-              name="email"
-              component={CustomField}
-              placeholder="email"
-              type="email"
-            />
-            <CustomField
-              label="Username"
-              name="username"
-              component={CustomField}
-              placeholder="username"
-              type="text"
-            />
-            <CustomField
-              label="Password"
-              name="password"
-              component={CustomField}
-              placeholder="password"
-              type="password"
-            />
-            <CustomField
-              label="Confirm Password"
-              name="confirmPassword"
-              component={CustomField}
-              placeholder="confirm"
-              type="password"
-            />
+            <Row>
+              <Col>
+                <h1>Register</h1>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <CustomField
+                  label="Email"
+                  name="email"
+                  component={CustomField}
+                  placeholder="email"
+                  type="email"
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <CustomField
+                  label="Username"
+                  name="username"
+                  component={CustomField}
+                  placeholder="username"
+                  type="text"
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <CustomField
+                  label="Password"
+                  name="password"
+                  component={CustomField}
+                  placeholder="password"
+                  type="password"
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <CustomField
+                  label="Confirm"
+                  name="confirmPassword"
+                  component={CustomField}
+                  placeholder="confirm"
+                  type="password"
+                />
+              </Col>
+            </Row>
           </Form>
-        )}
-      </Formik>
-    </>
+        </Container>
+      )}
+    </Formik>
   );
 }
