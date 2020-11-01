4d8c92a9f43e8b6f0bb6423b1795ef61cff34c86
brianthf <blora616@gmail.com>
2020-10-31 23:03:05 -0400

    fix index error

--- api/models.py
+++ api/models.py
@@ -40,5 +40,5 @@ user_role = db.Table('user_role',
                      db.Column('user_id', db.Integer, db.ForeignKey(
                          'user.id'), primary_key=True),
                      db.Column('role_id', db.Integer, db.ForeignKey(
-                         'role.id', primary_key=True))
+                         'role.id'), primary_key=True)
                      )
